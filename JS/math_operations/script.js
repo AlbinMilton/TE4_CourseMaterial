556515a30e903d1f4cb79b4a9a4d6aefc305ad7c
Albin Milton <albinmltn@gmail.com>
2025-09-16 13:33:55 +0200

    further work on JS

---
+++ JS/math_operations/script.js
@@ -0,0 +1,21 @@
+let a = 20;
+let b = 10;
+let c = 5;
+
+console.log(a + b + c);
+console.log(a - b - c);
+console.log(a * b * c);
+console.log(a / b / c);
+
+results = a + b * c;
+console.log(results);
+console.log(results > 50 && results < 100);
+
+let score = 85;
+if (score >= 90) {
+  console.log("Grade A");
+} else if (score >= 80) {
+  console.log("Grade B");
+} else if (score >= 70) {
+  console.log("Grade C");
+}
---
+++ JS/strings/script.js
@@ -0,0 +1,13 @@
+const firstName = "John";
+const lastName = "Doe";
+const quote = "'Jam a man of fortune, and J must seek my fortune'";
+console.log(`Hello, my name is ${firstName + " " + lastName}.`);
+console.log(quote);
+console.log(quote.length);
+
+const day = 16;
+const month = "September";
+const year = 2025;
+console.log(`Today, it's the ${day}th of ${month}, ${year}.`);
+const tomorrow = day + 1;
+console.log(`Tomorrow, it's the ${tomorrow}th of ${month}, ${year}.`);
