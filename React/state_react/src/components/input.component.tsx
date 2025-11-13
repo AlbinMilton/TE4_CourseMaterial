c13d2bb7411cefb26422bf66ec985ec109404fea
Albin Milton <albinmltn@gmail.com>
2025-11-13 13:04:50 +0100

    initial commit

---
+++ React/state_react/src/components/input.component.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const MyInput = () => {
+  const [text, setText] = useState<string>("InitialState");
+  console.log(text);
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setText(event.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Example - string in useState / Single Input</h2>
+      <input type="text" value={text} onChange={handleChange} />
+      <p>You typed: {text}</p>
+      <button onClick={() => setText("InitialState")}>Reset</button>
+    </div>
+  );
+};
+
+export default MyInput;
